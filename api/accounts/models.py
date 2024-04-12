3fc8b388c7e9ba969faae820b79637f236874abe
bilalabdelkadir <bilalbinabdelkadir>
2024-04-12 17:25:43 +0300

    feat: added account module and extended the default user model

---
+++ api/accounts/models.py
@@ -0,0 +1,75 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+
+class UserAccountManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, role, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+        if not first_name:
+            raise ValueError('Users must have a first name')
+        if not last_name:
+            raise ValueError('Users must have a last name')
+        if not role:
+            raise ValueError('Users must have a role')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+            role=role
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, first_name, last_name, role="admin", password=None):
+        user = self.create_user(
+            email,
+            first_name,
+            last_name,
+            role,
+            password
+        )
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class UserAccount(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    role = models.CharField(max_length=20, choices=(
+        ('admin', 'Admin'),
+        ('coach', 'Coach'),
+        ('agent', 'Agent'),
+        ('player', 'Player'),
+    ), default='player')
+    
+
+    objects = UserAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'role']
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def get_short_name(self):
+        return self.first_name
+
+    def has_role(self, role):
+        return self.role == role
+    
+    def has_module_perms(self, app_label):
+        return self.is_superuser
